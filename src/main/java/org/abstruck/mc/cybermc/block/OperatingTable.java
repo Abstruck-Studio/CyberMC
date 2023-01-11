c2d35fe610767071df52279c7effed9c82776821
Kom3ng <3262392248@qq.com>
2023-01-11 17:36:58 +0800

    up

--- src/main/java/org/abstruck/mc/cybermc/block/OperatingTable.java
+++ src/main/java/org/abstruck/mc/cybermc/block/OperatingTable.java
@@ -1,14 +1,11 @@
 package org.abstruck.mc.cybermc.block;
 
 
-import net.minecraft.block.BedBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.container.INamedContainerProvider;
-import net.minecraft.item.DyeColor;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -21,9 +18,9 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author Astrack
  */
-public class OperatingTable extends BedBlock {
+public class OperatingTable extends Block {
     public OperatingTable() {
-        super(DyeColor.WHITE,Properties.of(Material.STONE).harvestLevel(2));
+        super(Properties.of(Material.STONE).harvestLevel(2));
     }
 
     @Override
@@ -32,13 +29,12 @@ public class OperatingTable extends BedBlock {
     }
 
     @Override
-    @NotNull
-    public ActionResultType use(@NotNull BlockState blockState, @NotNull World world, @NotNull BlockPos blockPos, @NotNull PlayerEntity player, @NotNull Hand hand, @NotNull BlockRayTraceResult blockRayTraceResult) {
+    public @NotNull ActionResultType use(@NotNull BlockState blockState, World world, @NotNull BlockPos blockPos, @NotNull PlayerEntity player, @NotNull Hand hand, @NotNull BlockRayTraceResult blockRayTraceResult) {
         //判断是否是客户端玩家使用主手互动
         if (world.isClientSide && hand == Hand.MAIN_HAND){
             //获取手术台的方块实体
             OperatingTableTileEntity operatingTableTileEntity = (OperatingTableTileEntity) world.getBlockEntity(blockPos);
-            NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) operatingTableTileEntity, packetBuffer->{
+            NetworkHooks.openGui((ServerPlayerEntity) player, operatingTableTileEntity, packetBuffer->{
                 assert operatingTableTileEntity != null;
                 //把方块实体的坐标放进packetBuffer里
                 packetBuffer.writeBlockPos(operatingTableTileEntity.getBlockPos());
@@ -46,5 +42,6 @@ public class OperatingTable extends BedBlock {
         }
 
         return ActionResultType.SUCCESS;
+
     }
 }
